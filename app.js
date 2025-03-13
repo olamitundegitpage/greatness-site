eb03c2e4f5f3b337188d8016d323ce5e905472ec
olamitunde <tunsdemeritus@gmail.com>
2025-03-14 03:02:19 +0900

    greatness

---
+++ app.js
@@ -0,0 +1,11 @@
+// alert("hello")
+// const close = document.querySelector(".close");
+// // const open = document.querySelector(".ham");
+// const menu = document.querySelector(".enu");
+
+// close.addEventListener("click", () => {
+//     menu.style.visibility = "hidden";
+//   });
+//   open.addEventListener("click", () => {
+//     menu.style.visibility = "visible";
+//   });
